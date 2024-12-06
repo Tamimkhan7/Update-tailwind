c02869ca1efe3155b68656218c7562d34c26b60a
Tamim khan <mdtamimkhan842@gmail.com>
2024-12-06 10:46:12 +0600

    added tailwind learnirning

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    // for customization of the size or anything when we will define custome value 
+    screens: {
+      sm: '490px'
+    },
+    colors: {
+      'red': '#f1c40f',
+    },
+    fontFamily: {
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
